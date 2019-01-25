19ba5a081a15beff47d8822a4bfe9d2636d74367
Cameron Butler <cbutler2018@gmail.com>
2019-01-24 21:45:08 -0500

    Add product schema & basic queries

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,11 +1,20 @@
 /* eslint-disable import/prefer-default-export */
 
-const info = () => 'API Version 1';
+const infoRes = () => 'API Version 1';
 const users = async (parent, args, context) => context.db.query.users();
 const roles = async (parent, args, context) => context.db.query.roles();
+const products = async (parent, args, context) => context.db.query.products();
+const productsForService = async (parent, args, context, info) => context.db.query.products({
+  where: {
+    active: args.active,
+    forService: args.service
+  }
+}, info);
 
 export default {
-  info,
+  info: infoRes,
   users,
-  roles
+  roles,
+  products,
+  productsForService
 };
